fcb615be6b9f685ed170c084e8ee463e0cac4913
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-09-01 01:26:59 +0800

    完善Drawer的布局

--- app/src/main/java/sweet/wong/sweetnote/DrawerView.kt
+++ app/src/main/java/sweet/wong/sweetnote/DrawerView.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
 class DrawerView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : LinearLayout(context, attrs, defStyleAttr) {
+) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     init {
         LayoutInflater.from(context).inflate(R.layout.layout_drawer, this)
