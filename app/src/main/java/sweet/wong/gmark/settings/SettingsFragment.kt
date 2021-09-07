25bde9858ef0bc392bd1b8c13a0dd799d5e4be06
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-09-08 00:57:55 +0800

    [Feature] Add pref_settings.xml

--- app/src/main/java/sweet/wong/gmark/settings/SettingsFragment.kt
+++ app/src/main/java/sweet/wong/gmark/settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package sweet.wong.gmark.settings
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import sweet.wong.gmark.R
 
@@ -8,5 +10,11 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.pref_settings, rootKey)
+
+        val prefTextSize = findPreference<EditTextPreference>("text_size")
+        prefTextSize?.setOnBindEditTextListener {
+            it.inputType = InputType.TYPE_NUMBER_FLAG_DECIMAL
+        }
     }
+
 }
