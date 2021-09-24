4c604234555044ea34f8e1278c5abd3219f5de6e
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-09-25 00:15:53 +0800

    [Test] Complete git log test

--- app/src/test/java/sweet/wong/gmark/git/LogTest.kt
+++ app/src/test/java/sweet/wong/gmark/git/LogTest.kt
@@ -1,15 +1,38 @@
 package sweet.wong.gmark.git
 
+import com.blankj.utilcode.util.TimeUtils
 import org.junit.Test
 
 class LogTest {
 
     @Test
-    fun log() {
+    fun log() = timeCost {
         val revCommits = git.log().call()
         revCommits.forEachIndexed { index, revCommit ->
-            println(revCommit.shortMessage)
-            println()
+            revCommit.apply {
+                println(
+                    """
+                        index: $index
+                        name: $name
+                        id: $id
+                        type: $type
+                        firstByte: $firstByte
+                        authorIdent: $authorIdent
+                        commitTime: $commitTime, ${TimeUtils.millis2String((commitTime * 1000L))}
+                        committerIdent: $committerIdent
+                        encoding: $encoding
+                        encodingName: $encodingName
+                        footerLines: $footerLines
+                        fullMessage: ${fullMessage.trimIndent()}
+                        parentCount: $parentCount
+                        parents: $parents
+                        rawBuffer: $rawBuffer
+                        shortMessage: $shortMessage
+                        tree: $tree
+                        
+                    """.trimIndent()
+                )
+            }
         }
     }
 
