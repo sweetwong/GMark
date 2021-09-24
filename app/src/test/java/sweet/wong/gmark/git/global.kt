4d24d0b24737121bac2c0dcce656d0256ea1f09a
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-09-25 00:04:38 +0800

    [Test] Optimize the way to get the root directory

--- app/src/test/java/sweet/wong/gmark/git/global.kt
+++ app/src/test/java/sweet/wong/gmark/git/global.kt
@@ -3,9 +3,8 @@ package sweet.wong.gmark.git
 import org.eclipse.jgit.api.Git
 import java.io.File
 
-const val rootPath = "D:\\Project\\Mine\\GMark"
-
-val git: Git get() = Git.open(File(rootPath))
+val git: Git
+    get() = Git.open(File(System.getProperty("user.dir"), ".."))
 
 inline fun timeCost(action: () -> Unit) {
     val start = System.currentTimeMillis()
