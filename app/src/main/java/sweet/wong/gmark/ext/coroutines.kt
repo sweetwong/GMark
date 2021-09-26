baf7eb39f4a28b29e421ad3048dc1362cc79f023
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-09-26 18:36:52 +0800

    [Feature] Add CoroutineExceptionHandler to remove boilerplate try catch code

---
+++ app/src/main/java/sweet/wong/gmark/ext/coroutines.kt
@@ -0,0 +1,16 @@
+package sweet.wong.gmark.ext
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.Dispatchers
+import sweet.wong.gmark.core.toast
+
+private val exceptionHandler = CoroutineExceptionHandler { _, e ->
+    e.printStackTrace()
+    toast(e)
+}
+
+val Dispatchers.IO_CATCH
+    get() = Dispatchers.IO + exceptionHandler
+
+val Dispatchers.MAIN_CATCH
+    get() = Dispatchers.Main + exceptionHandler
--- app/src/main/java/sweet/wong/gmark/newrepo/NewRepoViewModel.kt
+++ app/src/main/java/sweet/wong/gmark/newrepo/NewRepoViewModel.kt
@@ -3,19 +3,15 @@ package sweet.wong.gmark.newrepo
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import kotlinx.coroutines.Dispatchers
-import sweet.wong.gmark.core.toast
 import sweet.wong.gmark.data.DaoManager
 import sweet.wong.gmark.data.Repo
+import sweet.wong.gmark.ext.IO_CATCH
 
 class NewRepoViewModel : ViewModel() {
 
-    fun addNewRepo(repo: Repo) = liveData(Dispatchers.IO) {
-        try {
-            DaoManager.repoDao.insertAll(repo)
-            emit(Unit)
-        } catch (e: Exception) {
-            toast("Add new repo failed", e)
-        }
+    fun addNewRepo(repo: Repo) = liveData(Dispatchers.IO_CATCH) {
+        DaoManager.repoDao.insertAll(repo)
+        emit(Unit)
     }
 
 }
