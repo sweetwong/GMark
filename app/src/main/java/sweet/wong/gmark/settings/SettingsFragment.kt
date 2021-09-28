eb93cca637051c1b2bf925d1137dcf28476de9ef
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-09-28 16:38:50 +0800

    [Auth] Optimize github oauth

--- app/src/main/java/sweet/wong/gmark/settings/SettingsFragment.kt
+++ app/src/main/java/sweet/wong/gmark/settings/SettingsFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.text.InputType
 import androidx.preference.EditTextPreference
 import androidx.preference.ListPreference
-import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import com.blankj.utilcode.util.AppUtils
 import com.blankj.utilcode.util.GsonUtils
+import com.blankj.utilcode.util.RegexUtils
 import com.google.firebase.auth.OAuthCredential
 import com.google.firebase.auth.OAuthProvider
 import com.google.firebase.auth.ktx.auth
@@ -35,50 +35,58 @@ class SettingsFragment : PreferenceFragmentCompat() {
             true
         }
 
-        val prefGithub = findPreference<Preference>(getString(R.string.pref_github_auth))
-        prefGithub?.setOnPreferenceClickListener {
-            val email = "yuanlai5chaos@gmail.com"
-            val provider = OAuthProvider.newBuilder("github.com")
-                .addCustomParameter("login", email)
-                .setScopes(listOf("repo"))
-
-            Firebase.auth
-                .startActivityForSignInWithProvider(requireActivity(), provider.build())
-                .addOnSuccessListener { authResult ->
-                    val userInfo = authResult.additionalUserInfo
-                        ?: return@addOnSuccessListener
-                    val profile = StringBuilder().apply {
-                        userInfo.profile?.forEach {
-                            append("${it.key}: ${it.value}\n")
-                        }
-                    }
-
-                    val token = (authResult.credential as? OAuthCredential)?.accessToken
-                        ?: return@addOnSuccessListener
-
-                    val avatar = authResult.additionalUserInfo?.profile?.get("avatar_url")
-                            as? String
-
-                    log(
-                        """
-                            isNewUser: ${userInfo.isNewUser}
-                            profile: $profile
-                            providerId: ${userInfo.providerId}
-                            username: ${userInfo.username}
-                            token: ${(authResult.credential as? OAuthCredential)?.accessToken}
-                        """.trimIndent()
-                    )
-
-                    val data = GithubAuthData(email, token, avatar)
-                    SPUtils.putString(SPConstant.GITHUB_AUTH_DATA, GsonUtils.toJson(data))
-                }
-                .addOnFailureListener {
-                    toast("失败", it)
-                }
-
-
+        val prefGithub = findPreference<EditTextPreference>(getString(R.string.pref_github_auth))
+        prefGithub?.setOnPreferenceChangeListener { preference, email ->
+            if (email !is String || !RegexUtils.isEmail(email)) {
+                toast("Please input valid email text")
+                return@setOnPreferenceChangeListener false
+            }
+            startGithubOAuth(email, preference as EditTextPreference)
             true
         }
     }
+
+    private fun startGithubOAuth(email: String, preference: EditTextPreference) {
+        val provider = OAuthProvider.newBuilder("github.com")
+            .addCustomParameter("login", email)
+            .setScopes(listOf("repo"))
+
+        Firebase.auth
+            .startActivityForSignInWithProvider(requireActivity(), provider.build())
+            .addOnSuccessListener { authResult ->
+                val userInfo = authResult.additionalUserInfo ?: return@addOnSuccessListener
+                val profile = StringBuilder().apply {
+                    userInfo.profile?.forEach {
+                        append("${it.key}: ${it.value}\n")
+                    }
+                }
+
+                val token = (authResult.credential as? OAuthCredential)?.accessToken
+                    ?: return@addOnSuccessListener
+
+                val avatar = authResult.additionalUserInfo?.profile?.get("avatar_url") as? String
+
+                log(
+                    """
+                        isNewUser: ${userInfo.isNewUser}
+                        profile: $profile
+                        providerId: ${userInfo.providerId}
+                        username: ${userInfo.username}
+                        token: ${(authResult.credential as? OAuthCredential)?.accessToken}
+                    """.trimIndent()
+                )
+
+                val data = GithubAuthData(email, token, avatar)
+                SPUtils.putString(SPConstant.GITHUB_AUTH_DATA, GsonUtils.toJson(data))
+
+                toast("Auth success")
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+                toast("失败", e)
+                preference.text = "\"{email}\" auth failed"
+            }
+    }
+
 
 }
