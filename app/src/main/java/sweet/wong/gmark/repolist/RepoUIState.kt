e02bba92397bc943ec0bb4ad8c6ac44ca8784d7c
sweetwang <39878472+sweetwong@users.noreply.github.com>
2021-10-07 18:46:03 +0800

    [UI] Optimize repository list update recyclerview animation

--- app/src/main/java/sweet/wong/gmark/repolist/RepoUIState.kt
+++ app/src/main/java/sweet/wong/gmark/repolist/RepoUIState.kt
@@ -12,4 +12,19 @@ class RepoUIState(val repo: Repo) : UIState() {
 
     var statusText = ""
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as RepoUIState
+
+        if (repo != other.repo) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return repo.hashCode()
+    }
+
 }
